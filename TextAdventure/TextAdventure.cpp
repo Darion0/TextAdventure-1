4fc7c369353590957d11e77444fe2eab52489e6a
Max Nanda <maxwellnanda@gmail.com>
2017-10-12 14:22:30 -0400

    basis for game

--- TextAdventure/TextAdventure.cpp
+++ TextAdventure/TextAdventure.cpp
@@ -13,84 +13,140 @@ int main()
 	string location = "start";
 	int health = 3;
 	string userChoice;
-	string keyeast = "no";
 	string weapon = "no";
 	string keyend = "no";
 
 	cout << "What is your name?";
-	cin >> name;
+	std::cin >> name;
 
-	if (location == "start")
+
+	while (userChoice != "q" && userChoice != "Q")
 	{
-		cout << "Alright, " << name << ". You find your self in a dark room. There is a door to our North, South, East and West.\n";
-		cin >> userChoice;
-	}
-
-	if (userChoice == "West" || userChoice == "west" || userChoice == "w")
-	{
-		cout << "There is a chest in the middle of the room and door behind it.\n";
-	}
-
-	if (userChoice == "East" || userChoice == "east" || userChoice == "e")
-	{
-		cout << "There is a ladder and a dark cave towards the back of the room.\n";
-	}
-
-	if (userChoice == "North" || userChoice == "north" || userChoice == "n")
-	{
-		location = "north";
-		cout << "You stand at the entrance of a long hallway with two doors on each side. You think you saw something move at the end of the hallway.\n";
-		cin >> userChoice;
-	}
-
-
-	if (userChoice == "start" || userChoice == "beginning" || userChoice == "b")
-	{
-		location = "start";
-		cout << "You find your self in a dark room. There is a door to our North, South, East and West.\n";
-		cin >> userChoice;
-	}
-	
-	if (userChoice == "south" || userChoice == "South" || userChoice == "s")
-	{
-		location = "south";
-		cout << "There is a monster in this room. It approaches you. What do you do?\n";
-		cin >> userChoice;
-
-		if (userChoice == "start" || userChoice == "beginning" || userChoice == "b")
+		if (location == "start")
 		{
-			location = "start";
-			cout << "You find your self in a dark room. There is a door to our North, South, East and West.\n";
-			cin >> userChoice;
-		}
+			cout << "Alright, " << name << ". You find your self in a dark room. There is a door to our North, South, East and West.\n";
+			std::cin >> userChoice;
 
-		if (userChoice == "attack" || userChoice == "Attack" || userChoice == "a" && weapon == "gun")
-		{
-			cout << "You shot and killed the monster! The monster's body falls to the floor and you notice a door at the end of the hallway.\n";
-			cin >> userChoice;
-			if (userChoice == "open" || userChoice == "Open" && keyend == "yes")
+			if (userChoice == "West" || userChoice == "west" || userChoice == "w")
 			{
-				cout << "Congrats! You have beat my game!\n";
-				cin >> userChoice;
+				location = "west";
 			}
+
+			else if (userChoice == "East" || userChoice == "east" || userChoice == "e")
+			{
+				location = "east";
+			}
+
+			else if (userChoice == "North" || userChoice == "north" || userChoice == "n")
+			{
+				location = "north";
+			}
+
+			else if (userChoice == "south" || userChoice == "South" || userChoice == "s")
+			{
+				location = "south";
+			}
+
 			else
 			{
-				cout << "You must not have the right key or any key at all.\n";
+				cout << "I don't understand.\n";
 			}
+
 		}
-		else
+
+		if (location == "West" || location == "west" || location == "w")
 		{
-			cout << "You weren't strong enough to defeat the monster, and he killed you.\n";
-			health = health - 3;
+			location = "west";
+			cout << "There is a chest in the middle of the room and door behind it.\n";
+			std::cin >> userChoice;
+
+			if (userChoice == "open chest" || userChoice == "oc")
+			{
+				cout << "You have found a small knife in the chest, you're attack is stronger now.\n";
+				weapon = "small knife";
+				std::cin >> userChoice;
+			}
+
+			else if (userChoice == "open door" || userChoice == "od")
+			{
+				location = "west door";
+			}
+
+			else if (userChoice == "start" || userChoice == "Start" || userChoice == "s")
+			{
+				location = "start";
+			}
+
+			else
+			{
+				cout << "I don't understand.\n";
+			}
+
+		}
+
+		if (location == "east"|| "e")
+		{
+			location = "east";
+			cout << "There is a ladder and a dark cave towards the back of the room.\n";
+			std::cin >> userChoice;
+
+			if (userChoice == "climb ladder" || "c" || "l")
+			{
+				location = "highRoom";
+			}
+
+			else if (userChoice == "cave" || "ca")
+			{
+				location = "cave";
+			}
+
+			else if (userChoice == "start" || "Start" || "s")
+			{
+				location = "start";
+			}
+
+			else
+			{
+				cout << "I don't understand.\n";
+			}
+
+		}
+
+		if (location == "north")
+		{
+			location = "north";
+			cout << "You stand at the entrance of a long hallway with two doors on each side. You think you saw something move at the end of the hallway.\n";
+			std::cin >> userChoice;
+
+			if (userChoice == "start" || "s")
+			{
+				location = "start";
+			}
+
+		}
+
+		if (location == "south")
+		{
+			location = "south";
+			cout << "There is a huge monster in this room. It has huge claws and teeth, and looks very hard to kill. It approaches you. What do you do?\n";
+			std::cin >> userChoice;
+
+			if (userChoice == "start" || userChoice == "beginning" || userChoice == "s")
+			{
+				location = "start";
+				cout << "You find your self in a dark room. There is a door to our North, South, East and West.\n";
+				std::cin >> userChoice;
+			}
+
+			
 		}
 	}
+
 
 	if (health == 0)
 	{
 		cout << "Game over, you lose.\n";
-		cin >> userChoice;
+		userChoice = "q";
 	}
-
-    return 0;
+	return 0;
 }
-
